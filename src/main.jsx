9c0790a0683359e380bebe31df2166f61b34670e
sunsunjp <crypto.currency.91400@gmail.com>
2025-12-14 23:20:27 +0900

    Add deployment files for Cloudflare Pages

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import SlidePuzzle from './slide-puzzle.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <SlidePuzzle />
+  </React.StrictMode>,
+)
